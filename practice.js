c39fa29bfa68bd0ead48af4e61838539870cad4c
mustanjid <mustanjid.se@gmail.com>
2022-02-25 12:23:42 +0600

    ES practice code and modified overall practice

--- practice.js
+++ practice.js
@@ -217,3 +217,105 @@ const contacts = [
   console.log(lookUpProfile("Kristian", "lastName"));
   console.log(lookUpProfile("Bob", "number"));
   //console.log(contacts[0].hasOwnProperty('firstName'))
+
+  // var vs let
+  // var numArray = [];
+  // for(var i=0; i < 5; i++)
+  // {
+  //   numArray.push(i);
+  // }
+  // console.log(numArray);
+  // console.log(i);
+
+let printNumTwo;
+for (let i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo());
+//console.log(i);
+
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+checkScope();
+
+//Mutate an Array Declared with const
+const mutableArray = [1, 2, 3];
+//mutableArray = [4, 5 , 6] // initializing an array to a const array is unacceptable
+mutableArray[1] = 3; //but changing the value of an index is acceptable
+console.log(mutableArray);
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+
+  for(let i=0, j=s.length-1; i < j; i++, j--)
+  {
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+  }
+
+  // Only change code above this line
+}
+editInPlace();
+console.log(s);
+
+//Prevent Object Mutation
+function freezeObj()
+{
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  }
+
+Object.freeze(MATH_CONSTANTS);
+try{
+  MATH_CONSTANTS.PI = 99;
+}catch(ex)
+{
+  console.log(ex);
+}
+return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+console.log(PI);
+
+//Use Arrow Functions to Write Concise Anonymous Functions
+//Js allows us to write shorter version of function call by using arrow or we can call arrow function
+
+//normal function
+var normalFunc = function(){
+  let value = "Normal Function assigned to a variable"
+  return value;
+}
+console.log(normalFunc());
+
+var shorterFun = ()=>{
+  return "shorter fucntion assigned to a variable";
+}
+console.log(shorterFun());
+
+//if shorter fucn is to return only a value than we can omit the curly braces
+let shorterFuncOnlyReturnValue = ()=>"this is a returning value from shorterFuncOnlyReturnValue";
+console.log(shorterFuncOnlyReturnValue());
+
+//Write Arrow Functions with Parameters
+let addNum = (a, b)=>a+b;
+console.log(addNum(1, 2));
+
+//if an arrow function has only one parameter then we can omit the curly braces
+const doubler = item => item*2;
+console.log(doubler(4));
+
+const myContact = (arr1, arr2) => arr1.concat(arr2);
+console.log(myContact([1, 2], [3, 4, 5]));
