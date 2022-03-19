a3fb7e163c6a4e29bd16b5e2624602dec77a45e8
mustanjid <mustanjid.se@gmail.com>
2022-03-19 22:54:05 +0600

    intermdeiate algo solve code added

---
+++ Intermediate Algo with Js/diff-two-arrays.js
@@ -0,0 +1,9 @@
+function diffArray(arr1, arr2) {
+  //const newArr = [];
+  return arr1.concat(arr2).filter(item => !arr2.includes(item) || !arr1.includes(item))
+  //return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
