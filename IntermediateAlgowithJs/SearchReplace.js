294edc87d9fa56a6358ad72145da78af563a7fe3
Mustanjid <mustanjid.se@gmail.com>
2022-11-22 23:17:27 +0600

    SearchReplace problem solving done

--- IntermediateAlgowithJs/SearchReplace.js
+++ IntermediateAlgowithJs/SearchReplace.js
@@ -10,19 +10,21 @@ Third argument is what you will be replacing the second argument with (after).
 */
 
 function myReplace(str, before, after) {
-    let matchWord = str.match(before);
-    if(matchWord == matchWord.toString().toUpperCase())
-    {
-      console.log("Ok");
-    }
-    else{
-      console.log("Not Ok")
-    }
-    
-    let replaceWord = str.replace(matchWord, after)
-    return replaceWord;
+  //let matchWord = str.match(before).toString();
+  //console.log(matchWord);
+  if(after[0] === after[0].toUpperCase())
+  {
+    after = after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  else if (before[0] === before[0].toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1);
   }
   
-console.log(myReplace("Let us go to the store", "store", "mall"));
+  let replaceSentence = str.replace(before, after)
+  return replaceSentence;
+}
+
+// console.log(myReplace("Let us go to the store", "store", "mall"));
 console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
 console.log(myReplace("His name is Tom", "Tom", "john"));
+//console.log(myReplace("I think we should look up there", "up", "Down"));
