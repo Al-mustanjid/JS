8c136747e4617dc51ce181b4e5abfa86397b52b9
Mustanjid <mustanjid.se@gmail.com>
2022-11-30 23:31:53 +0600

    new solved problem added

---
+++ IntermediateAlgowithJs/dna-pairing.js
@@ -0,0 +1,32 @@
+/*Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array. */
+
+function pairElement(str) {
+    let pairarr = [];
+    for(let i=0; i < str.length; i++)
+    {
+        switch(str[i]){
+            case "A":
+                pairarr.push(['A', 'T']);
+                break;
+            case "T":
+                pairarr.push(["T", "A"]);
+                break;
+            case "G":
+                pairarr.push(["G", "C"]);
+                break;
+            case "C":
+                pairarr.push(["C", "G"]);
+                break;
+        }  
+    }
+   
+    return pairarr;
+  }
+
+  console.log(pairElement("GCG"));
